bf70c9d68de7c884705ffbdc630d6598c5e058ac
caetanus <marcelo@fiveti.com>
2012-11-09 20:52:47 -0200

    initial commit

---
+++ __main__.py
@@ -0,0 +1,1 @@
+from socketqueue import *
---
+++ setup.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+        name='sockqueue',
+        version='0.1.5',
+        long_description=open('README.txt').read(),
+        packages=find_packages()
+        )
